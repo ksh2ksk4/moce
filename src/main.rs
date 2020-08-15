1b2d33c99f8bc5be4dac55861fc467240cd2e6e2
KISHI Kousuke <ksh2ksk4@gmail.com>
2020-08-16 07:08:46 +0900

    Enable to move the cursor

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,27 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
+struct Cursor {
+    x: u16,
+    y: u16
+}
+
 fn main() {
     let (terminal_size_x, terminal_size_y) = termion::terminal_size().unwrap();
+    // カーソルの座標を保持する構造体も (1, 1)-based にしておく
+    let mut cursor = Cursor {
+        x: 1,
+        y: 1
+    };
+
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
 
     // cursor::Goto() は (1, 1)-based
     write!(
         stdout,
-        "{}{}{}{}{}{}{}",
+        "{}{}{}{}{}{}",
         clear::All,
-        cursor::Hide,
         cursor::Goto(1, terminal_size_y),
         color::Bg(color::Yellow),
         " ".repeat(terminal_size_x.try_into().unwrap()),
@@ -27,14 +37,34 @@ fn main() {
     for c in stdin.keys() {
         match c.unwrap() {
             Key::Ctrl('q') => break,
-            _ => print!("<other>")
+            Key::Left => {
+                cursor.x -= 1;
+                print!("{}", cursor::Goto(cursor.x, cursor.y));
+            },
+            Key::Right => {
+                cursor.x += 1;
+                print!("{}", cursor::Goto(cursor.x, cursor.y));
+            },
+            Key::Up => {
+                cursor.y -= 1;
+                print!("{}", cursor::Goto(cursor.x, cursor.y));
+            },
+            Key::Down => {
+                cursor.y += 1;
+                print!("{}", cursor::Goto(cursor.x, cursor.y));
+            },
+            _ => {
+                print!("<other>");
+                cursor.x += 7;
+                print!("{}", cursor::Goto(cursor.x, cursor.y));
+            }
         }
 
         stdout.flush().unwrap();
     }
 
     // Postprocess
-    write!(stdout, "{}{}{}", clear::All, cursor::Goto(1, 1), cursor::Show).unwrap();
+    write!(stdout, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
 
     // アプリ終了時に termion が後始末をしてターミナルを canonical mode に戻してくれる
 }
