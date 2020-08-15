f09b78d328671a12ffd9ed1acd0afda39a3bde7f
KISHI Kousuke <ksh2ksk4@gmail.com>
2020-08-16 06:13:41 +0900

    Add some functions
    - Change the mode of a terminal into the raw mode
    - Exit on C-q
    - Clear a terminal when this app exit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    write!(
+        stdout,
+        "{}{}{}",
+        termion::clear::All,
+        termion::cursor::Hide,
+        // cursor::Goto() は (1, 1)-based
+        termion::cursor::Goto(1, 1)
+    ).unwrap();
+    stdout.flush().unwrap();
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Ctrl('q') => break,
+            _ => print!("<other>")
+        }
+
+        stdout.flush().unwrap();
+    }
+
+    // Postprocess
+    write!(
+        stdout,
+        "{}{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Show
+    ).unwrap();
+
+    // termion が後始末をしてくれるのでアプリ終了後にターミナルが raw mode のままということはない
 }
